7375a0665207a95e6294e865dca18150c02ace85
LadySerena <serena.tiede+github@gmail.com>
2026-01-09 18:13:39 -0600

    refactor: move enum/union to different module
    The enums and unions are very chonky, so I'm moving them to a different
    module. I'm trying to abstract away the more overwhelming details.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,11 +1,13 @@
-use std::{ffi::CStr, ptr::null_mut};
+use std::ptr::null_mut;
 
 use nix::errno::Errno;
 
-use crate::api::c_symbols::{
-    fatal_proc_unmounted, pids_fetch, pids_info, pids_result__bindgen_ty_1, procps_pids_new,
-    procps_pids_reap,
+use crate::api::{
+    c_symbols::{fatal_proc_unmounted, pids_fetch, pids_info, procps_pids_new, procps_pids_reap},
+    union::{read_from_union, Pid_result, Process_Info},
 };
+
+mod union;
 
 #[allow(dead_code)]
 #[allow(clippy::upper_case_acronyms)]
@@ -24,17 +26,6 @@ pub struct Pid_Counts {
     pub other: i32,
 }
 
-#[derive(Debug)]
-pub enum Pid_result {
-    char(i8),
-    int32(i32),
-    uint32(u32),
-    uint64(u64),
-    str(String),
-    real(f64),
-    empty(Option<()>),
-}
-
 impl Pid_Counts {
     fn from_ptr(ptr: *mut c_symbols::pids_counts) -> Self {
         let de_ref = unsafe { *ptr };
@@ -45,176 +36,6 @@ impl Pid_Counts {
             stopped: de_ref.stopped,
             zombied: de_ref.zombied,
             other: de_ref.other,
-        }
-    }
-}
-
-fn read_from_union(item: pids_item, result: pids_result__bindgen_ty_1) -> Pid_result {
-    unsafe {
-        match item {
-            pids_item::PIDS_noop => Pid_result::empty(None),
-            pids_item::PIDS_extra => Pid_result::empty(None),
-            pids_item::PIDS_ADDR_CODE_END => todo!(),
-            pids_item::PIDS_ADDR_CODE_START => todo!(),
-            pids_item::PIDS_ADDR_CURR_EIP => todo!(),
-            pids_item::PIDS_ADDR_CURR_ESP => todo!(),
-            pids_item::PIDS_ADDR_STACK_START => todo!(),
-            pids_item::PIDS_AUTOGRP_ID => todo!(),
-            pids_item::PIDS_AUTOGRP_NICE => todo!(),
-            pids_item::PIDS_CGNAME => todo!(),
-            pids_item::PIDS_CGROUP => {
-                let meep = CStr::from_ptr(result.str_);
-                Pid_result::str(meep.to_string_lossy().to_string())
-            }
-            pids_item::PIDS_CGROUP_V => todo!(),
-            pids_item::PIDS_CMD => {
-                let meep = CStr::from_ptr(result.str_);
-                Pid_result::str(meep.to_string_lossy().to_string())
-            }
-            pids_item::PIDS_CMDLINE => todo!(),
-            pids_item::PIDS_CMDLINE_V => todo!(),
-            pids_item::PIDS_ENVIRON => todo!(),
-            pids_item::PIDS_ENVIRON_V => todo!(),
-            pids_item::PIDS_EXE => todo!(),
-            pids_item::PIDS_EXIT_SIGNAL => todo!(),
-            pids_item::PIDS_FLAGS => todo!(),
-            pids_item::PIDS_FLT_MAJ => todo!(),
-            pids_item::PIDS_FLT_MAJ_C => todo!(),
-            pids_item::PIDS_FLT_MAJ_DELTA => todo!(),
-            pids_item::PIDS_FLT_MIN => todo!(),
-            pids_item::PIDS_FLT_MIN_C => todo!(),
-            pids_item::PIDS_FLT_MIN_DELTA => todo!(),
-            pids_item::PIDS_ID_EGID => todo!(),
-            pids_item::PIDS_ID_EGROUP => todo!(),
-            pids_item::PIDS_ID_EUID => todo!(),
-            pids_item::PIDS_ID_EUSER => todo!(),
-            pids_item::PIDS_ID_FGID => todo!(),
-            pids_item::PIDS_ID_FGROUP => todo!(),
-            pids_item::PIDS_ID_FUID => todo!(),
-            pids_item::PIDS_ID_FUSER => todo!(),
-            pids_item::PIDS_ID_LOGIN => todo!(),
-            pids_item::PIDS_ID_PGRP => todo!(),
-            pids_item::PIDS_ID_PID => Pid_result::int32(result.s_int),
-            pids_item::PIDS_ID_PPID => todo!(),
-            pids_item::PIDS_ID_RGID => todo!(),
-            pids_item::PIDS_ID_RGROUP => todo!(),
-            pids_item::PIDS_ID_RUID => todo!(),
-            pids_item::PIDS_ID_RUSER => todo!(),
-            pids_item::PIDS_ID_SESSION => todo!(),
-            pids_item::PIDS_ID_SGID => todo!(),
-            pids_item::PIDS_ID_SGROUP => todo!(),
-            pids_item::PIDS_ID_SUID => todo!(),
-            pids_item::PIDS_ID_SUSER => todo!(),
-            pids_item::PIDS_ID_TGID => todo!(),
-            pids_item::PIDS_ID_TID => todo!(),
-            pids_item::PIDS_ID_TPGID => todo!(),
-            pids_item::PIDS_IO_READ_BYTES => todo!(),
-            pids_item::PIDS_IO_READ_CHARS => todo!(),
-            pids_item::PIDS_IO_READ_OPS => todo!(),
-            pids_item::PIDS_IO_WRITE_BYTES => todo!(),
-            pids_item::PIDS_IO_WRITE_CBYTES => todo!(),
-            pids_item::PIDS_IO_WRITE_CHARS => todo!(),
-            pids_item::PIDS_IO_WRITE_OPS => todo!(),
-            pids_item::PIDS_LXCNAME => todo!(),
-            pids_item::PIDS_MEM_CODE => todo!(),
-            pids_item::PIDS_MEM_CODE_PGS => todo!(),
-            pids_item::PIDS_MEM_DATA => todo!(),
-            pids_item::PIDS_MEM_DATA_PGS => todo!(),
-            pids_item::PIDS_MEM_RES => todo!(),
-            pids_item::PIDS_MEM_RES_PGS => todo!(),
-            pids_item::PIDS_MEM_SHR => todo!(),
-            pids_item::PIDS_MEM_SHR_PGS => todo!(),
-            pids_item::PIDS_MEM_VIRT => todo!(),
-            pids_item::PIDS_MEM_VIRT_PGS => todo!(),
-            pids_item::PIDS_NICE => todo!(),
-            pids_item::PIDS_NLWP => todo!(),
-            pids_item::PIDS_NS_CGROUP => todo!(),
-            pids_item::PIDS_NS_IPC => todo!(),
-            pids_item::PIDS_NS_MNT => todo!(),
-            pids_item::PIDS_NS_NET => todo!(),
-            pids_item::PIDS_NS_PID => todo!(),
-            pids_item::PIDS_NS_TIME => todo!(),
-            pids_item::PIDS_NS_USER => todo!(),
-            pids_item::PIDS_NS_UTS => todo!(),
-            pids_item::PIDS_OOM_ADJ => todo!(),
-            pids_item::PIDS_OOM_SCORE => todo!(),
-            pids_item::PIDS_PRIORITY => todo!(),
-            pids_item::PIDS_PRIORITY_RT => todo!(),
-            pids_item::PIDS_PROCESSOR => todo!(),
-            pids_item::PIDS_PROCESSOR_NODE => todo!(),
-            pids_item::PIDS_RSS => todo!(),
-            pids_item::PIDS_RSS_RLIM => todo!(),
-            pids_item::PIDS_SCHED_CLASS => todo!(),
-            pids_item::PIDS_SD_MACH => todo!(),
-            pids_item::PIDS_SD_OUID => todo!(),
-            pids_item::PIDS_SD_SEAT => todo!(),
-            pids_item::PIDS_SD_SESS => todo!(),
-            pids_item::PIDS_SD_SLICE => todo!(),
-            pids_item::PIDS_SD_UNIT => todo!(),
-            pids_item::PIDS_SD_UUNIT => todo!(),
-            pids_item::PIDS_SIGBLOCKED => todo!(),
-            pids_item::PIDS_SIGCATCH => todo!(),
-            pids_item::PIDS_SIGIGNORE => todo!(),
-            pids_item::PIDS_SIGNALS => todo!(),
-            pids_item::PIDS_SIGPENDING => todo!(),
-            pids_item::PIDS_SMAP_ANONYMOUS => todo!(),
-            pids_item::PIDS_SMAP_HUGE_ANON => todo!(),
-            pids_item::PIDS_SMAP_HUGE_FILE => todo!(),
-            pids_item::PIDS_SMAP_HUGE_SHMEM => todo!(),
-            pids_item::PIDS_SMAP_HUGE_TLBPRV => todo!(),
-            pids_item::PIDS_SMAP_HUGE_TLBSHR => todo!(),
-            pids_item::PIDS_SMAP_LAZY_FREE => todo!(),
-            pids_item::PIDS_SMAP_LOCKED => todo!(),
-            pids_item::PIDS_SMAP_PRV_CLEAN => todo!(),
-            pids_item::PIDS_SMAP_PRV_DIRTY => todo!(),
-            pids_item::PIDS_SMAP_PRV_TOTAL => todo!(),
-            pids_item::PIDS_SMAP_PSS => todo!(),
-            pids_item::PIDS_SMAP_PSS_ANON => todo!(),
-            pids_item::PIDS_SMAP_PSS_FILE => todo!(),
-            pids_item::PIDS_SMAP_PSS_SHMEM => todo!(),
-            pids_item::PIDS_SMAP_REFERENCED => todo!(),
-            pids_item::PIDS_SMAP_RSS => todo!(),
-            pids_item::PIDS_SMAP_SHR_CLEAN => todo!(),
-            pids_item::PIDS_SMAP_SHR_DIRTY => todo!(),
-            pids_item::PIDS_SMAP_SWAP => todo!(),
-            pids_item::PIDS_SMAP_SWAP_PSS => todo!(),
-            pids_item::PIDS_STATE => todo!(),
-            pids_item::PIDS_SUPGIDS => todo!(),
-            pids_item::PIDS_SUPGROUPS => todo!(),
-            pids_item::PIDS_TICS_ALL => Pid_result::uint64(result.ull_int),
-            pids_item::PIDS_TICS_ALL_C => todo!(),
-            pids_item::PIDS_TICS_ALL_DELTA => todo!(),
-            pids_item::PIDS_TICS_BEGAN => todo!(),
-            pids_item::PIDS_TICS_BLKIO => todo!(),
-            pids_item::PIDS_TICS_GUEST => todo!(),
-            pids_item::PIDS_TICS_GUEST_C => todo!(),
-            pids_item::PIDS_TICS_SYSTEM => Pid_result::uint64(result.ull_int),
-            pids_item::PIDS_TICS_SYSTEM_C => todo!(),
-            pids_item::PIDS_TICS_USER => Pid_result::uint64(result.ull_int),
-            pids_item::PIDS_TICS_USER_C => todo!(),
-            pids_item::PIDS_TIME_ALL => todo!(),
-            pids_item::PIDS_TIME_ALL_C => todo!(),
-            pids_item::PIDS_TIME_ELAPSED => todo!(),
-            pids_item::PIDS_TIME_START => todo!(),
-            pids_item::PIDS_TTY => todo!(),
-            pids_item::PIDS_TTY_NAME => todo!(),
-            pids_item::PIDS_TTY_NUMBER => todo!(),
-            pids_item::PIDS_UTILIZATION => todo!(),
-            pids_item::PIDS_UTILIZATION_C => todo!(),
-            pids_item::PIDS_VM_DATA => todo!(),
-            pids_item::PIDS_VM_EXE => todo!(),
-            pids_item::PIDS_VM_LIB => todo!(),
-            pids_item::PIDS_VM_RSS => todo!(),
-            pids_item::PIDS_VM_RSS_ANON => todo!(),
-            pids_item::PIDS_VM_RSS_FILE => todo!(),
-            pids_item::PIDS_VM_RSS_LOCKED => todo!(),
-            pids_item::PIDS_VM_RSS_SHARED => todo!(),
-            pids_item::PIDS_VM_SIZE => todo!(),
-            pids_item::PIDS_VM_STACK => todo!(),
-            pids_item::PIDS_VM_SWAP => todo!(),
-            pids_item::PIDS_VM_USED => todo!(),
-            pids_item::PIDS_VSIZE_BYTES => todo!(),
-            pids_item::PIDS_WCHAN_NAME => todo!(),
         }
     }
 }
@@ -237,19 +58,26 @@ pub fn scan_procs(items: Vec<pids_item>) -> pids_fetch {
     // drop container?
     let counts = Pid_Counts::from_ptr(pids.counts);
     let loop_bound = usize::try_from(counts.total).expect("convert total to usize");
+    print_header(&items);
     // pointer arithmetic see ~/Code/ladyserena/ctop/main.c
     for n in 0..loop_bound {
+        let mut entry: Vec<Process_Info> = Vec::with_capacity(items.len());
         let stack = unsafe { (*(*pids.stacks.add(n))).head };
         for i in 0..items.len() {
             let inner = unsafe { *stack.add(i) };
             let key: pids_item = inner.item;
             let result = inner.result;
-            let data = read_from_union(key, result);
-            println!("{:#?} {:#?}", key, data)
+            entry.push(Process_Info {
+                item: key,
+                value: read_from_union(key, result),
+            });
         }
+        println!("{:#?}", entry);
     }
     pids.to_owned()
 }
+
+fn print_header(items: &[pids_item]) {}
 
 fn reap(info: &mut pids_info) -> &pids_fetch {
     unsafe {
