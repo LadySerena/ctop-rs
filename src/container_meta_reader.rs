9696b37b38619f45f131222e01dc79b62c9038e9
LadySerena <serena.tiede+github@gmail.com>
2026-02-15 19:47:34 -0600

    feat: implement pid2container meta
    I added imports for clap and the containerd client. I might replace
    containerd with a generic CRI client since all runtimes for k8s must
    implement CRI.
    
    It's very hacky right now, but it'll do for now.

---
+++ src/container_meta_reader.rs
@@ -0,0 +1,110 @@
+use std::path::Path;
+
+use containerd_client::{
+    connect,
+    services::v1::{containers_client::ContainersClient, GetContainerRequest},
+    tonic::transport::Channel,
+    tonic::Request,
+    with_namespace,
+};
+
+use crate::{errors::InitError, pids_item, read::ProcessInfo, ContainerMetaReader};
+
+#[derive(Debug)]
+pub struct ContainerMeta {
+    process_id: i32,
+    name: String,
+    namespace: String,
+}
+
+pub struct ContainerdReader {
+    client: ContainersClient<Channel>,
+}
+
+impl ContainerMetaReader for ContainerdReader {
+    async fn new(runtime_endpoint: String) -> Result<Self, InitError>
+    where
+        Self: Sized,
+    {
+        let channel = match connect(runtime_endpoint).await {
+            Ok(c) => c,
+            Err(err) => return Err(InitError::ContainerClientInit(err)),
+        };
+
+        let client = ContainersClient::new(channel);
+
+        Ok(Self { client })
+    }
+
+    async fn proc_to_container(
+        mut self,
+        infos: ProcessInfo,
+    ) -> Result<Vec<ContainerMeta>, crate::errors::ReadError> {
+        let Some(pid_index) = infos
+            .items
+            .iter()
+            .position(|x| *x == pids_item::PIDS_ID_PID)
+        else {
+            return Err(crate::errors::ReadError::MissingItem(
+                pids_item::PIDS_ID_PID,
+            ));
+        };
+        let Some(cgroup_index) = infos
+            .items
+            .iter()
+            .position(|x| *x == pids_item::PIDS_CGROUP_V)
+        else {
+            return Err(crate::errors::ReadError::MissingItem(
+                pids_item::PIDS_CGROUP_V,
+            ));
+        };
+        let mut output: Vec<ContainerMeta> = Vec::new();
+        for info in infos.procs {
+            let pid = match info.get(pid_index).expect("all processes to have pid") {
+                crate::read::Value::Int32(p) => *p,
+                _ => unreachable!(),
+            };
+            let cgroup = match info
+                .get(cgroup_index)
+                .expect("all processes to have a cgroup")
+            {
+                crate::read::Value::Str(c) => c.to_string(),
+                _ => unreachable!(),
+            };
+            if !cgroup.contains("kubelet-kubepods.slice") {
+                continue;
+            }
+            let cgroup_path = Path::new(&cgroup);
+            // we have to trim the resulting path
+            // kubelet.slice/kubelet-kubepods.slice/kubelet-kubepods-burstable.slice/kubelet-kubepods-burstable-pod70c196014b615996a2893e59ef9bd41d.slice/cri-containerd-8f92d57269cccc8617178621006662f45c086e1251ba6ce5508b7f6165d8a2a1.scope
+            // we want the last part but without the leading cri-containerd- and the trailing .scope
+            let container_id = cgroup_path
+                .iter()
+                .find(|x| x.to_str().unwrap().starts_with("cri-containerd"))
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .trim_start_matches("cri-containerd-")
+                .trim_end_matches(".scope");
+            let request = GetContainerRequest {
+                id: container_id.to_string(),
+            };
+            let response = self
+                .client
+                .get(with_namespace!(request, "k8s.io"))
+                .await
+                .unwrap();
+            let labels = response.into_inner().container.unwrap().labels;
+            let pod_name = labels.get("io.kubernetes.pod.name").unwrap();
+            let pod_namespace = labels.get("io.kubernetes.pod.namespace").unwrap();
+
+            output.push(ContainerMeta {
+                process_id: pid,
+                name: pod_name.to_string(),
+                namespace: pod_namespace.to_string(),
+            });
+        }
+
+        Ok(output)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+use std::future::Future;
+
 pub use bindings::pids_item;
+use container_meta_reader::ContainerMeta;
 use errors::{InitError, ReadError};
 use read::ProcessInfo;
 
@@ -10,6 +13,7 @@ use read::ProcessInfo;
 mod bindings {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
+pub mod container_meta_reader;
 mod errors;
 mod init;
 pub mod proc_reader;
@@ -21,4 +25,14 @@ pub trait ProcReader {
         Self: Sized;
 
     fn scan_pids(&self) -> Result<ProcessInfo, ReadError>;
+}
+
+pub trait ContainerMetaReader {
+    fn new(runtime_endpoint: String) -> impl Future<Output = Result<Self, InitError>>
+    where
+        Self: Sized;
+    fn proc_to_container(
+        self,
+        info: ProcessInfo,
+    ) -> impl Future<Output = Result<Vec<ContainerMeta>, ReadError>>;
 }
