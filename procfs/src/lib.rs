7e9e2cef316ed085f7c190eb3d91aeab8b96437c
LadySerena <serena.tiede+github@gmail.com>
2026-08-10 12:01:16 -0500

    proc-lib: redeclare enums within crate
    Trying to separate the details from the c lib to the rust crate.

--- procfs/src/lib.rs
+++ procfs/src/lib.rs
@@ -11,62 +11,62 @@ pub struct PidItem {
 /// Enum containing fields from proc_pid_stat
 /// for more information consult `man proc_pid_stat`
 pub enum PidStatItems {
-    /// Process id (stat)
-    Pid(i32),
-    /// process state (stat)
-    State(String),
-    /// parent PID (stat)
-    Ppid(i32),
-    /// parent group (stat)
-    Pgrp(i32),
-    /// minor page fault (no disk reload) (stat)
-    Minflt(u64),
-    /// child minor page fault (stat)
-    Cminflt(u64),
-    /// Major fault (reload page from disk) (stat)
-    Majflt(u64),
-    /// child Major fault (reload page from disk) (stat)
-    Cmajflt(u64),
-    /// user mode time (stat)
-    Utime(u64),
-    /// kernel mode time (stat)
-    Stime(u64),
-    /// child user mode time (stat)
-    Cutime(i64),
-    /// child kernel mode time (stat)
-    Cstime(i64),
-    /// number of OS threads (stat)
-    NumThreads(i64),
-    /// Time process started after boot (clock ticks) (stat)
-    Starttime(u128),
-    /// virtual memory size (bytes) (stat)
-    Vsize(u64),
-    /// resident set size (number of pages) (stat)
-    Rss(i64),
-    /// soft limit in bytes of RSS for process (stat)
-    Rsslim(u64),
-    /// block i/o delays clock ticks (stat)
-    DelayacctBlkioTicks(u128),
-    /// number of clock ticks a guest VM ran on a virtual CPU(stat)
-    GuestTime(u64),
-    /// guest time of process's children (stat)
-    CguestTime(i64),
-    /// cgroup name (cgroup)
-    CgroupName(String),
-    /// (cmdline)
-    CmdLine(String),
+    /// Process id (stat) i32
+    Pid,
+    /// process state (stat) string
+    State,
+    /// parent PID (stat) i32
+    Ppid,
+    /// parent group (stat) i32
+    Pgrp,
+    /// minor page fault (no disk reload) (stat) u64
+    Minflt,
+    /// child minor page fault (stat) u64
+    Cminflt,
+    /// Major fault (reload page from disk) (stat) u64
+    Majflt,
+    /// child Major fault (reload page from disk) (stat) u64
+    Cmajflt,
+    /// user mode time (stat) u64
+    Utime,
+    /// kernel mode time (stat) u64
+    Stime,
+    /// child user mode time (stat) i64
+    Cutime,
+    /// child kernel mode time (stat) i64
+    Cstime,
+    /// number of OS threads (stat) i64
+    NumThreads,
+    /// Time process started after boot (clock ticks) (stat) u128
+    Starttime,
+    /// virtual memory size (bytes) (stat) u64
+    Vsize,
+    /// resident set size (number of pages) (stat) i64
+    Rss,
+    /// soft limit in bytes of RSS for process (stat) u64
+    Rsslim,
+    /// block i/o delays clock ticks (stat) u128
+    DelayacctBlkioTicks,
+    /// number of clock ticks a guest VM ran on a virtual CPU(stat) u64
+    GuestTime,
+    /// guest time of process's children (stat) i64
+    CguestTime,
+    /// cgroup name (cgroup) string
+    CgroupName,
+    /// (cmdline) string
+    CmdLine,
 }
 
 /// enum for items in proc_pid_io
 pub enum PidIOItems {
-    /// (io)
-    ReadBytes(i64),
-    /// (io)
-    WriteBytes(i64),
-    /// (io)
-    SysCallRead(i64),
-    /// (io)
-    SysCallWrite(i64),
+    /// (io) i64
+    ReadBytes,
+    /// (io) i64
+    WriteBytes,
+    /// (io) i64
+    SysCallRead,
+    /// (io) i64
+    SysCallWrite,
 }
 
 /// enum containing identifiers for /proc/net/dev
